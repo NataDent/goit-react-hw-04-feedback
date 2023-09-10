81de9b6c0d6fb5634b50198ca16ebafa97b5b528
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-10 21:06:53 +0300

    Buttons don`t work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,27 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const options = ['good', 'neutral', 'bad'];
 
   const onLeaveFeedback = e => {
-    const name = e.target.textContent.toLowerCase();
-
-    switch (name) {
-      case 'good':
-        setGood(state => state + 1);
-        break;
-      case 'neutral':
-        setNeutral(state => state + 1);
-        break;
-      case 'bad':
-        setBad(state => state + 1);
-        break;
-      default:
-        return;
-    }
+    setFeedback(prevState => [...prevState, ...feedback]);
   };
+  const countTotalFeedback = () =>
+    Object.values(feedback).reduce((acc, el) => acc + el, 0);
 
-  const options = [good, neutral, bad];
-  const countTotalFeedback = good + neutral + bad;
-
-  const countPositivePercentage = Math.round(good / countTotalFeedback) * 100;
+  const countPositivePercentage = Math.round(
+    (feedback.good / countTotalFeedback()) * 100
+  );
 
   return (
     <>
@@ -40,9 +31,9 @@ export const App = () => {
       <Section title="Statistics">
         {countTotalFeedback > 0 ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
             total={countTotalFeedback}
             positivePercentage={countPositivePercentage}
           />
